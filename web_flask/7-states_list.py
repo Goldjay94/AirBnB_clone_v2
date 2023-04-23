c9539341344e3e1b972752a98a91510786449fa4
Goldjay94 <genevieveezeh786@gmail.com>
2023-04-23 18:15:17 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""
-script that starts a Flask web application
-"""
+"""This script starts a Flask web application"""
+
+
 from flask import Flask, render_template
-from models import *
+from models.state import State
 from models import storage
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def index():
-    """renders an HTML with a list of states"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """ This function closes the session"""
+    storage.close()
+
+
+@app.route('/states_list')
+def show_states():
+    """ This function renders the template 7-states_list.html"""
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """Close the SQLAlchemy Session"""
-    storage.close()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0')
